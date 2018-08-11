81f4fec0a51fd46b8ab6c1091d7b4af63be03672
Dhruba Saha <dkdsaha@gmail.com>
2018-08-11 18:38:41 -0400

    Data change

--- src/JavaHomeWork/WhileLoopEx4.java
+++ src/JavaHomeWork/WhileLoopEx4.java
@@ -3,7 +3,7 @@ package JavaHomeWork;
 public class WhileLoopEx4 {
 
 	public static void main(String[] args) {
-		double a=1.0;
+		double a=2.0;
 		while(a<=2.2) {
 			System.out.println(a);
 			a=a+0.1;
